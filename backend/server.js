2f2612e611a58900b600016ab37963747de90862
Baskar <br.baskar1@gmail.com>
2023-12-08 23:56:04 +0530

    User model created

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
+const app = express();
 import dotenv from "dotenv";
+dotenv.config();
 import userRoutes from './routes/userRoutes.js'
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
 
-dotenv.config();
+connectDB();
 
-const app = express();
 const port = process.env.PORT || 5000;
 
 app.use('/api/users', userRoutes);
