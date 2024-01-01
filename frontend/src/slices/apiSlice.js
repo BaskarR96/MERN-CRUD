b062ee46a37dc3b7b0081443d775d3e9e6b10f4e
Baskar R <87247127+BaskarR96@users.noreply.github.com>
2024-01-01 13:38:29 +0530

    Update apiSlice.js

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({ 
-    baseUrl: 'https://mern-crud-backend-psi.vercel.app/',
+    baseUrl: 'https://mern-crud-backend-psi.vercel.app',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
