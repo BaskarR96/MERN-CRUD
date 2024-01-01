b14b0417ddac50ad269cd064f08229d2b2981a2e
Baskar R <87247127+BaskarR96@users.noreply.github.com>
2024-01-01 13:48:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,10 @@ connectDB();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: ['https://mern-crud-frontend-five.vercel.app'],
-    methods: ["POST", "GET", "PUT", "DELETE"],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin'],
-    credentials: true
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
 }))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
