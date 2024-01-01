89b430d20a8b30549f8f0fcaf635624a3e21bf64
Baskar R <87247127+BaskarR96@users.noreply.github.com>
2024-01-01 13:55:26 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(cors({
     "origin": "*",
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "preflightContinue": false,
-    "optionsSuccessStatus": 204
+    "optionsSuccessStatus": 200
 }))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
