e3a324f4cdc20b7c56dbd0b61716b911f2cf03f2
Baskar <br.baskar1@gmail.com>
2024-01-02 19:34:39 +0530

    env setup

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: `${import.meta.env.NODE_ENV === "production" ? import.meta.env.VITE_API_BASE_URL : `http://localhost:5000`}`
+    baseUrl: `${import.meta.env.MODE === "production" ? import.meta.env.VITE_API_BASE_URL : `http://localhost:5000`}`
 });
 
 export const apiSlice = createApi({
