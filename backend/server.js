14c073992aa1a7bcf62955b099f564eefa63916d
Baskar R <87247127+BaskarR96@users.noreply.github.com>
2024-01-02 10:29:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,9 +29,7 @@ const port = process.env.PORT || 5000;
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // *.domain.com update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept","Content-disposition");
-  res.header("Access-Control-Allow-Headers","Origin, X-Requeted-With, Content-Type, Accept,Content-disposition, Authorization, RBR");
   res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
-  if (req.method === "OPTIONS") { return res.status(200).end(); }
   next();
 });
 
