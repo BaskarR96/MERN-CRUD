107e549678102c56e308e253323604975927c79e
Baskar <br.baskar1@gmail.com>
2024-01-02 18:53:36 +0530

    env setup

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,8 @@ connectDB();
 
 const port = process.env.PORT || 5000;
 
-app.use(cors())
-// app.options('*', cors());
- 
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,6 +1,8 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
-const baseQuery = fetchBaseQuery({ baseUrl: 'https://mern-crud-backend-psi.vercel.app' });
+const baseQuery = fetchBaseQuery({
+    baseUrl: `${import.meta.env.NODE_ENV === "production" ? import.meta.env.VITE_API_BASE_URL : `http://localhost:5000`}`
+});
 
 export const apiSlice = createApi({
     baseQuery,
