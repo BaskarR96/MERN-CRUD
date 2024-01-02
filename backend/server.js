69ba78b1f4e1807ba5ffc5d7fce6e5cee107e41d
Baskar R <87247127+BaskarR96@users.noreply.github.com>
2024-01-02 15:07:53 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,28 +11,14 @@ connectDB();
 
 const port = process.env.PORT || 5000;
 
-// app.use(cors({
-//     "origin": "https://mern-crud-frontend-five.vercel.app",
-//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-//     "preflightContinue": false,
-//     "optionsSuccessStatus": 200
-// }))
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "https://mern-crud-frontend-five.vercel.app"); // update to match the domain you will make the request from
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
+app.use(cors({
+    "origin": "https://www.mern-crud-frontend-five.vercel.app",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 200
+}))
 // app.options('*', cors());
  
-// app.use("/public", express.static(path.join(__dirname, 'public')));
- 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // *.domain.com update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept","Content-disposition");
-  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
-  next();
-});
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
