1026ec4139b311033a371a372b0cfc88b4bd0f29
Baskar R <87247127+BaskarR96@users.noreply.github.com>
2024-01-02 18:24:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,7 @@ connectDB();
 
 const port = process.env.PORT || 5000;
 
-// app.use(cors({
-//     "origin": "*",
-//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-//     "preflightContinue": false,
-//     "optionsSuccessStatus": 200
-// }))
+app.use(cors())
 // app.options('*', cors());
  
 app.use(express.json());
